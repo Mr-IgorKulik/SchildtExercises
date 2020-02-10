72b54cc967b0284a5edcd4c152c9b64bcfdfc61f
Mr-IgorKulik <mr.igor.kulik@gmail.com>
2020-02-10 17:15:57 +0200

    first commit

---
+++ ex_2.1/Echo.java
@@ -0,0 +1,10 @@
+class Echo {
+    public static void main(String[] args) {
+        double time = 14.4;
+        double dist = time / 2 * 1100;
+
+        System.out.println("Расстояние до большого объекта составит " + 
+                            dist + " футов, если время возврата эхо составляет " +
+                            time + " секунд.");
+    }
+}
